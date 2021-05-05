8a2a2b028556354afe332309cc47e81dd53b8626
Alan Watson <alanowatson@gmail.com>
2021-05-05 10:45:25 -0400

    First tries at JQuery

---
+++ playground.js
@@ -0,0 +1,30 @@
+$('.red h3').text('Abacus Central');
+$('.red h3').text('Abacus Central');
+$('h1').text('Hello World');
+$('.blue h3').text('Grenadine Dreams');
+$('.white h3').html('<code>CODE</code> Central');
+$('body').css('font-family', 'sans-serif');
+$('body').css('background-color', '#777');
+$('section').css('display', 'flex');
+
+$('.card')
+  .css('border', '1px solid black')
+  .css('padding', '8px')
+  .css('margin', '8px')
+  .css('flex', '1');
+
+$('.red').css('background', 'red').css('color', 'white');
+
+$('.blue').css('background', 'blue').css('color', 'white');
+
+$('.white').css('background', 'white');
+
+$('code').css('font-size', '1.7em');
+
+$('.lead-cards p').css('font-family', 'cursive');
+
+$('section:nth-of-type(2)')
+  .css('transition', 'transform 3s ease')
+  .css('transform', 'rotate(30deg) scale(.5)');
+
+$('.deprecated').remove();
